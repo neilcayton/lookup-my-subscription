6e8a2a44add543d4c9d7f9234ecbd2c03db1ad6d
neilcayton <neilcayton03@gmail.com>
2025-03-11 20:41:57 +0800

    Update Firebase configuration

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,15 +13,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyBHDND0_omqqeVmVHgn6ff3ZJ96KY1SDoI",
   authDomain: "lookup-88a6f.firebaseapp.com",
   projectId: "lookup-88a6f",
-  storageBucket: "lookup-88a6f.firebasestorage.app",
+  storageBucket: "lookup-88a6f.appspot.com",
   messagingSenderId: "1040585476012",
   appId: "1:1040585476012:web:55b01d09332706f75a1c09",
   measurementId: "G-MVRVBX0DQF"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getStorage(app);
+export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
